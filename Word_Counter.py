8ad1103e42bedd0f3061e0c8192d16b722ab6215
Jash Ambaliya <jashambaliya1@gmail.com>
2023-08-28 21:06:48 +0530

    word counter

---
+++ Word_Counter.py
@@ -0,0 +1,15 @@
+def word_counter(text):
+    # Split the text into words
+    words = text.split()
+
+    # Count the number of words
+    word_count = len(words)
+
+    return word_count
+
+# Input text
+input_text = input("Enter a sentence or paragraph: ")
+
+# Calculate and display word count
+count = word_counter(input_text)
+print(f"Word Count: {count}")
